7882d9ce096ea46fd34397d8eee5efc3c88b2470
KSHMR1337 <marko.zare1234@hotmail.com>
2023-09-23 22:34:50 +0200

    Updated tulip linux client script

--- tulip/linux_tulip_client.sh
+++ tulip/linux_tulip_client.sh
@@ -2,5 +2,22 @@
 
 set -e
 
-aa-complain `which tcpdump`
-sudo tcpdump -Z root -U -G 60 -w ./file-%H-%M-%S.pcap -z ./send_pcap.sh
+j=0
+cap=capture
+
+while :
+do
+
+
+        sudo rsync -Pav -e "ssh -i ./ctf.pem" ./*.pcap ctf@13.81.33.90:/home/ctf/tulip/tulip/services/test_pcap
+ tshark -i br-2e41af42b2d5 -i br-767059b5cc65 -i br-61f0a9730558 -i br-7680633083ab -i br-a142acb5cd25 -i br-45e6509cbfb1 -a duration:20 -w ./$cap$j.pcap          
+
+        sudo rsync -Pav -e "ssh -i ./ctf.pem" ./*.pcap ctf@13.81.33.90:/home/ctf/tulip/tulip/services/test_pcap
+
+
+        rm *.pcap
+
+        j=$((j+1))
+
+done
+
